5304eab77fc8bf225ef51066b090dbcb7079dc6d
Bryon F <bryon790@revature.net>
2023-01-18 20:04:53 -0600

    some progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Router, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RestaurantComponent } from './restaurant/restaurant.component';
+import { RestaurantComponent } from './components/restaurant/restaurant.component';
 import { ReviewComponent } from './components/review/review.component';
 import { LogInComponent } from './components/log-in/log-in.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { LogInComponent } from './components/log-in/log-in.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/log-in/log-in.component.ts
+++ src/app/components/log-in/log-in.component.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs';
+
 import { User } from 'src/app/models/user';
+import { LogInService } from 'src/app/services/log-in.service';
+import { TransferService } from 'src/app/services/transfer.service';
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
   styleUrls: ['./log-in.component.css']
 })
-export class LogInComponent {
-userLoggedIn = false;
-user = new User();
+export class LogInComponent implements OnInit {
+  userLoggedIn = false;
+  currentUser!: User;
+  loginForm: FormGroup = this.fb.group({});
+
+
+  constructor(private httpServ: LogInService, private router: Router,
+    private transfer: TransferService, private fb: FormBuilder) { }
+
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email], { updateOn: 'blur' }],
+      password: ['', Validators.required],
+    });
+  }
+
+  fieldInvalid(formControl: AbstractControl){
+    return formControl.invalid && formControl.dirty;
+  }
+
+  onSubmit(): void{
+    const email = this.loginForm.get('email')?.value;
+    const password = this.loginForm.get('password')?.value;
+
+    this.httpServ.logIn(email,password).pipe(
+      tap(response => this.currentUser = new User())
+    ).subscribe((response) => this.userLoggedIn = true);
+  }
 
 }
+
+
+
